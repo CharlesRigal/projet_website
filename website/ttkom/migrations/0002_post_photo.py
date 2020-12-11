8c08954c2024f843da793bc266ad076ca435ef2a
rigal <charlesrigal123@gmail.com>
2020-12-11 15:27:55 +0100

    add logout

--- website/ttkom/migrations/0002_post_photo.py
+++ website/ttkom/migrations/0002_post_photo.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ttkom', '0001_initial'),
+        ("ttkom", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='post',
-            name='photo',
-            field=models.ImageField(null=True, upload_to='post-photo'),
+            model_name="post",
+            name="photo",
+            field=models.ImageField(null=True, upload_to="post-photo"),
         ),
     ]
--- website/ttkom/urls.py
+++ website/ttkom/urls.py
@@ -1,4 +1,14 @@
 from django.urls import path
 from .views import index, post
+from django.contrib.auth import views as auth_views
 
-urlpatterns = [path("", index, name="index"), path("post/<int:pk>", post, name="post")]
+urlpatterns = [
+    path("", index, name="index"),
+    path("post/<int:pk>", post, name="post"),
+    path("connextion/", auth_views.LoginView.as_view(), name="login"),
+    path(
+        "deconnection/",
+        auth_views.LogoutView.as_view(template_name="registration/logged-out.html"),
+        name="logout",
+    ),
+]
